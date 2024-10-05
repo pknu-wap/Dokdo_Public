e7cb8ef7943107e9707a63529d8699fc3918e91e
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-10-05 22:26:03 +0900

    Feat: 닉네임 입력 후 인트로 애니메이션 페이지로 이동
    검은 바탕에 인트로 문구 fade in, fade out 애니메이션 후 게임 첫 뉴스 화면으로 자동 이동

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import StartPage from './pages/StartPage';
+import IntroPage from './pages/IntroPage';
+import NewsPage from './pages/NewsPage';
 
 function Main() {
   return (
@@ -8,6 +10,8 @@ function Main() {
       <App>
         <Routes>
           <Route path="/" element={<StartPage />} />
+          <Route path="intro" element={<IntroPage />} />
+          <Route path="intro/news" element={<NewsPage />} />
         </Routes>
       </App>
     </BrowserRouter>
