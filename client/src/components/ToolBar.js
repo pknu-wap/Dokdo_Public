a12b7bd9200b9980a8844aaee265a6af340527c2
aranlll <163715414+aranlll@users.noreply.github.com>
2024-10-08 20:25:57 +0900

    Feat:툴 바 구현

---
+++ client/src/components/ToolBar.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import './ToolBar.css';
+
+function ToolBar() {
+  return (
+    <div className="Grid">
+      <Link to="/Room1" className="ToolBar" style={{ textDecoration: 'none', gridArea: 'a' }}>
+        Room1
+      </Link>
+      <Link to="/Room2Page" className="ToolBar" style={{ textDecoration: 'none', gridArea: 'b' }}>
+        Room2
+      </Link>
+      <Link to="/Room3Page" className="ToolBar" style={{ textDecoration: 'none', gridArea: 'c' }}>
+        Room3
+      </Link>
+      <div style={{ gridArea: 'f', backgroundColor: '#ffffff' }} />
+
+      <div className="Inventory" />
+    </div>
+  );
+}
+
+export default ToolBar;
