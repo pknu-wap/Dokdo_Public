cf3359178c6a5c38a9bbcd2d7b69b107a8c86f37
Chaeyeon Baek <qorcodus03@naver.com>
2024-10-08 21:26:44 +0900

    Chore: 프로젝트 초기 설정

---
+++ sever/src/main/java/com/example/rememberdokdo/RememberDokdoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rememberdokdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberDokdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberDokdoApplication.class, args);
+    }
+
+}
---
+++ sever/src/test/java/com/example/rememberdokdo/RememberDokdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rememberdokdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberDokdoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
