ae7848a0d049ae15978a4656405215cf058c0380
김수현 <suhyun_113@naver.com>
2024-10-12 04:46:46 +0900

    Feat: InventoryEntity에 @Getter와 @Setter 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
@@ -2,9 +2,13 @@ package com.example.rememberdokdo.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 // 인벤토리 기능에 필요할 변수들
 @Entity
+@Getter
+@Setter
 public class InventoryEntity {
     @Id
     private Long sessionId; // 세션 식별자
