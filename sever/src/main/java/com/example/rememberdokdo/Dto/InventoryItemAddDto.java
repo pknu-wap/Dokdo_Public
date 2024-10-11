80d18e41cbcdd39645a75ccddb054e4c033b9d8c
김수현 <suhyun_113@naver.com>
2024-10-12 05:20:20 +0900

    Refactor: 인벤토리 아이템 추가 DTO 파일 이름 변경

--- sever/src/main/java/com/example/rememberdokdo/Dto/InventoryItemAddDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/InventoryItemAddDto.java
@@ -1,6 +1,6 @@
 package com.example.rememberdokdo.Dto;
 
-public class InventoryItemDto {
+public class InventoryItemAddDto {
     private Long itemId; // 아이템 식별자
     private String itemName; // 아이템 이름
     private String itemDescription; // 아이템 설명
