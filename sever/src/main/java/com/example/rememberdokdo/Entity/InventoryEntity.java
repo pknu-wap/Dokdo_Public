c9e3a5c95bab4f9b32cba91eaa4a1f80da26ac0b
김수현 <suhyun_113@naver.com>
2024-10-12 04:45:55 +0900

    Feat: InventoryEntity에 @Entity와 @Id 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
@@ -1,7 +1,12 @@
 package com.example.rememberdokdo.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 // 인벤토리 기능에 필요할 변수들
+@Entity
 public class InventoryEntity {
+    @Id
     private Long sessionId; // 세션 식별자
     private Long itemId; // 아이템 식별자
     private String itemName; // 아이템 이름
