312db16ec7101b4e2b1e1cc3da51e56eb73d2367
김수현 <suhyun_113@naver.com>
2024-10-12 04:35:16 +0900

    Feat: 인벤토리 기능에 필요한 변수 추가

--- sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
@@ -1,4 +1,9 @@
 package com.example.rememberdokdo.Entity;
 
+// 인벤토리 기능에 필요할 변수들
 public class InventoryEntity {
+    private Long sessionId; // 세션 식별자
+    private Long itemId; // 아이템 식별자
+    private String itemName; // 아이템 이름
+    private String itemDescription; // 아이템 설명
 }
