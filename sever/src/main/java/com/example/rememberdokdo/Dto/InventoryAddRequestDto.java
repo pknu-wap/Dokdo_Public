258129829180a3422a308c86000a219e181e2fd8
김수현 <suhyun_113@naver.com>
2024-10-12 05:07:23 +0900

    Feat: 인벤토리 아이템 추가 요청 DTO 변수 추가

---
+++ sever/src/main/java/com/example/rememberdokdo/Dto/InventoryAddRequestDto.java
@@ -0,0 +1,8 @@
+package com.example.rememberdokdo.Dto;
+
+public class InventoryAddRequestDto {
+    private Long sessionId; // 세션 식별자(넣을지 고민중)
+    private Long itemId; // 아이템 식별자
+    private String itemName;
+    private String itemDescription;
+}
