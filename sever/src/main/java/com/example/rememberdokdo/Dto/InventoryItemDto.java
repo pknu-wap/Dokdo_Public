8427acd9dc6e33c288355c40e039a9965f8a7eef
김수현 <suhyun_113@naver.com>
2024-10-12 05:16:48 +0900

    Feat: 인벤토리 아이템 DTO 변수 추가

---
+++ sever/src/main/java/com/example/rememberdokdo/Dto/InventoryItemDto.java
@@ -0,0 +1,7 @@
+package com.example.rememberdokdo.Dto;
+
+public class InventoryItemDto {
+    private Long itemId; // 아이템 식별자
+    private String itemName; // 아이템 이름
+    private String itemDescription; // 아이템 설명
+}
