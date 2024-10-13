59ec49648d6b19ff67cb1e45945be300a85b41ff
김수현 <suhyun_113@naver.com>
2024-10-14 06:35:25 +0900

    Feat: 아이템 삭제 서비스 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Service/Inventory/InventoryDeleteItemsService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/Inventory/InventoryDeleteItemsService.java
@@ -1,7 +1,9 @@
 package com.example.rememberdokdo.Service.Inventory;
 
 import com.example.rememberdokdo.Dto.Inventory.ItemDeleteRequestDto;
+import com.example.rememberdokdo.Repository.Inventory.InventoryItemsRepository;
 import com.example.rememberdokdo.Repository.Inventory.InventoryRepository;
+import com.example.rememberdokdo.Repository.Inventory.ItemsRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,10 @@ import org.springframework.stereotype.Service;
 public class InventoryDeleteItemsService {
     @Autowired
     private InventoryRepository inventoryRepository;
+    @Autowired
+    private ItemsRepository itemsRepository;
+    @Autowired
+    private InventoryItemsRepository inventoryItemsRepository;
 
     @Transactional
     public void deleteItem(ItemDeleteRequestDto request) {
