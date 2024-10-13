4176daac5fd89838775ef94381a6f9355b0ea11a
김수현 <suhyun_113@naver.com>
2024-10-14 00:51:05 +0900

    Feat: ItemsEntity 변수 추가

---
+++ sever/src/main/java/com/example/rememberdokdo/Entity/ItemsEntity.java
@@ -0,0 +1,7 @@
+package com.example.rememberdokdo.Entity;
+
+public class ItemsEntity {
+    private Integer itemId; // 아이템 식별자
+    private String itemName; // 아이템 이름
+    private String itemDescription; // 아이템 설명
+}
