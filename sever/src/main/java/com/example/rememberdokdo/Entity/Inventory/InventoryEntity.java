f9946cd3886b28ffe19c405e348dc07878005ddd
김수현 <suhyun_113@naver.com>
2024-10-14 06:10:25 +0900

    Feat: 어노테이션 추가 및 주석 추가

--- sever/src/main/java/com/example/rememberdokdo/Entity/Inventory/InventoryEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/Inventory/InventoryEntity.java
@@ -3,12 +3,18 @@ package com.example.rememberdokdo.Entity.Inventory;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // 인벤토리 기능에 필요할 변수들
 @Entity
+@Data // Lombok이 자동으로 getter, setter, equals, hashcode, toString 등을 생성
+@NoArgsConstructor // 기본 생성자 생성
+@AllArgsConstructor // 모든 필드를 인자로 받는 생성자 생성
+@Builder // 빌더 패턴을 사용할 수 있도록 함
 @Table(name = "Inventory")
-@Data
 public class InventoryEntity {
     @Id
     private Integer inventoryId; // 인벤토리 식별자 (고유키)
--- sever/src/main/java/com/example/rememberdokdo/Entity/Inventory/InventoryItemsEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/Inventory/InventoryItemsEntity.java
@@ -3,11 +3,17 @@ package com.example.rememberdokdo.Entity.Inventory;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data // Lombok이 자동으로 getter, setter, equals, hashcode, toString 등을 생성
+@NoArgsConstructor // 기본 생성자 생성
+@AllArgsConstructor // 모든 필드를 인자로 받는 생성자 생성
+@Builder // 빌더 패턴을 사용할 수 있도록 함
 @Table(name = "InventoryItems")
-@Data
 public class InventoryItemsEntity {
     @Id
     private Integer inventoryItemsId; // 인벤토리 아이템 식별자
