28840e855a15c4c1ce7a4d4be372487975ab6146
김수현 <suhyun_113@naver.com>
2024-10-14 00:57:18 +0900

    Feat: inventoryItemsEntity 변수 추가

---
+++ sever/src/main/java/com/example/rememberdokdo/Entity/InventoryItemsEntity.java
@@ -0,0 +1,7 @@
+package com.example.rememberdokdo.Entity;
+
+public class InventoryItemsEntity {
+    private Integer inventoryItemsId; // 인벤토리 아이템 식별자
+    private Integer inventoryId; // 인벤토리 식별자
+    private Integer itemsId; // 아이템 식별자
+}
