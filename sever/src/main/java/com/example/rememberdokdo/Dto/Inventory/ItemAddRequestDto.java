dd609d34915b454ca8ef6c0ddb9632dac0f8d4f5
김수현 <suhyun_113@naver.com>
2024-10-14 01:04:18 +0900

    Refactor: DTO 파일 이름 수정

--- sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
@@ -1,6 +1,6 @@
 package com.example.rememberdokdo.Dto.Inventory;
 
-public class InventoryAddRequestDto {
+public class ItemAddRequestDto {
     private Long sessionId; // 세션 식별자(넣을지 고민중)
     private Long itemId; // 아이템 식별자
     private String itemName;
--- sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemDeleteRequestDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemDeleteRequestDto.java
@@ -1,6 +1,6 @@
 package com.example.rememberdokdo.Dto.Inventory;
 
-public class InventoryItemAddDto {
+public class ItemDeleteRequestDto {
     private Long itemId; // 아이템 식별자
     private String itemName; // 아이템 이름
     private String itemDescription; // 아이템 설명
