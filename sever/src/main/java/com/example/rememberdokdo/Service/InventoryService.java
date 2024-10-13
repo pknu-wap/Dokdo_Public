ceb1d500764875b3c1ed66aa222477e3588348e1
김수현 <suhyun_113@naver.com>
2024-10-14 04:12:21 +0900

    Chore: 아이템 추가 로직 주석 추가

--- sever/src/main/java/com/example/rememberdokdo/Service/InventoryService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/InventoryService.java
@@ -26,5 +26,17 @@ public class InventoryService {
         InventoryEntity inventory = inventoryRepository.findBySessionId(request.getSessionId()).orElse(null);
         // 아이템 식별자 존재 확인
         ItemsEntity items = itemsRepository.findById(request.getItemId()).orElse(null);
+
+        // 세션 식별자와 매핑된 인벤토리 식별자 존재 확인
+        // 인벤토리 생성x => 인벤토리 생성
+        // 인벤토리 생성o => 매핑된 인벤토리 식별자 조회
+
+        // 인벤토리에 아이템 식별자 존재 확인
+        // 아이템 식별자 존재o => 오류 메시지(중복일 경우)
+        // 아이템 식별자 존재x => 아이템 추가
+
+        // 조회한 인벤토리 위치에 아이템 추가
+
+        // DB 테이블 최신화
     }
 }
