c3222ea374ecd61e191febaece2e255b40b3892f
김수현 <suhyun_113@naver.com>
2024-10-14 03:53:30 +0900

    Feat: 아이템 추가 기능 세션 식별자 존재 확인 및 아이템 식별자 존재 확인 추가

--- sever/src/main/java/com/example/rememberdokdo/Service/InventoryService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/InventoryService.java
@@ -1,8 +1,12 @@
 package com.example.rememberdokdo.Service;
 
+import com.example.rememberdokdo.Dto.Inventory.ItemAddRequestDto;
+import com.example.rememberdokdo.Entity.Inventory.InventoryEntity;
+import com.example.rememberdokdo.Entity.Inventory.ItemsEntity;
 import com.example.rememberdokdo.Repository.Inventory.InventoryItemsRepository;
 import com.example.rememberdokdo.Repository.Inventory.InventoryRepository;
 import com.example.rememberdokdo.Repository.Inventory.ItemsRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +18,13 @@ public class InventoryService {
     private ItemsRepository itemsRepository;
     @Autowired
     private InventoryItemsRepository inventoryItemsRepository;
+
+    //아이템 추가
+    @Transactional
+    public void addItem(ItemAddRequestDto request) {
+        // 세션 식별자 존재 확인
+        InventoryEntity inventory = inventoryRepository.findBySessionId(request.getSessionId()).orElse(null);
+        // 아이템 식별자 존재 확인
+        ItemsEntity items = itemsRepository.findById(request.getItemId()).orElse(null);
+    }
 }
