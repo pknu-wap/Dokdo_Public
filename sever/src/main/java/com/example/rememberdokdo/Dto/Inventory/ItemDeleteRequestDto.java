f19b8b5a0f7b34388fef7ed6b6d6453a80734718
김수현 <suhyun_113@naver.com>
2024-10-14 01:35:59 +0900

    Feat: 아이템 삭제 요청 DTO 파일 변수 및 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemDeleteRequestDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemDeleteRequestDto.java
@@ -1,7 +1,9 @@
 package com.example.rememberdokdo.Dto.Inventory;
 
+import lombok.Data;
+
+@Data
 public class ItemDeleteRequestDto {
-    private Long itemId; // 아이템 식별자
-    private String itemName; // 아이템 이름
-    private String itemDescription; // 아이템 설명
+    private String sessionId; // 세션 식별자
+    private Integer itemId; // 아이템 식별자
 }
