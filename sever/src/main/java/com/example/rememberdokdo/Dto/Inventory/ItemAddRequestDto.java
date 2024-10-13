645903eb66bf60b64143eb0a22d66673dada59cb
김수현 <suhyun_113@naver.com>
2024-10-14 01:32:59 +0900

    Feat: 아이템 추가 요청 DTO 파일 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
@@ -1,5 +1,8 @@
 package com.example.rememberdokdo.Dto.Inventory;
 
+import lombok.Data;
+
+@Data
 public class ItemAddRequestDto {
     private String sessionId; // 세션 식별자(넣을지 고민중)
     private Integer itemId; // 아이템 식별자
