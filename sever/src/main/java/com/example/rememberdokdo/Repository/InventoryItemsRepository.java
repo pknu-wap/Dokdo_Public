07332729d831fa004368a26bd5324820dace9c70
김수현 <suhyun_113@naver.com>
2024-10-14 02:47:27 +0900

    Feat: 인벤토리 ID와 아이템 ID 동시 조회 메서드 추가

--- sever/src/main/java/com/example/rememberdokdo/Repository/InventoryItemsRepository.java
+++ sever/src/main/java/com/example/rememberdokdo/Repository/InventoryItemsRepository.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Repository;
 // 인벤토리 아이템
 @Repository
 public interface InventoryItemsRepository extends JpaRepository<InventoryItemsEntity, Integer> {
+    // 인벤토리 식별자와 아이템 식별자 동시 조회
+    InventoryItemsEntity findInventoryIdAndItemsId(Integer inventoryId, Integer itemsId);
 }
 
