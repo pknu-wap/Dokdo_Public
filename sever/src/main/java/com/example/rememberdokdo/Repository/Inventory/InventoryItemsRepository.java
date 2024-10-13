2cbef7c180c5fea9f391a01f97ec7d4aa9d4dcc0
김수현 <suhyun_113@naver.com>
2024-10-14 07:22:11 +0900

    Feat: 아이템 삭제 null 예외 처리를 하기 위해 Optional 추가

--- sever/src/main/java/com/example/rememberdokdo/Repository/Inventory/InventoryItemsRepository.java
+++ sever/src/main/java/com/example/rememberdokdo/Repository/Inventory/InventoryItemsRepository.java
@@ -4,11 +4,14 @@ import com.example.rememberdokdo.Entity.Inventory.InventoryItemsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // 인벤토리 아이템
 @Repository
 public interface InventoryItemsRepository extends JpaRepository<InventoryItemsEntity, Integer> {
     // 인벤토리 식별자와 아이템 식별자 동시 조회
-    InventoryItemsEntity findByInventoryIdAndItemsId(Integer inventoryId, Integer itemsId);
+    Optional<InventoryItemsEntity> findByInventoryIdAndItemsId(Integer inventoryId, Integer itemId);
+
 
     // 인벤토리 식별자에 아이템 식별자가 존재하는지 여부를 확인하는 메서드
     boolean existsByInventoryIdAndItemsId(Integer inventoryId, Integer itemsId);
