8c60376c300a337571ebfa22b04d73dd49c2fe7b
김수현 <suhyun_113@naver.com>
2024-10-14 03:19:37 +0900

    Feat: 세션 식별자에서 인벤토리 식별자로 변경

--- sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class ItemAddRequestDto {
-    private String sessionId; // 세션 식별자(넣을지 고민중)
+    private Integer inventoryId; // 인벤토리 식별자
     private Integer itemId; // 아이템 식별자
 }
