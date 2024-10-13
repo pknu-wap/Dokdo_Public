4cd15f022399616af4dd7a9c1df5ffa2a9b57c75
김수현 <suhyun_113@naver.com>
2024-10-14 00:35:48 +0900

    Feat: InventoryEntity 변수 변경

--- sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/InventoryEntity.java
@@ -11,8 +11,6 @@ import lombok.Setter;
 @Setter
 public class InventoryEntity {
     @Id
-    private Long sessionId; // 세션 식별자
-    private Long itemId; // 아이템 식별자
-    private String itemName; // 아이템 이름
-    private String itemDescription; // 아이템 설명
+    private Integer inventoryId; // 인벤토리 식별자 (고유키)
+    private String sessionId; // 세션 식별자
 }
