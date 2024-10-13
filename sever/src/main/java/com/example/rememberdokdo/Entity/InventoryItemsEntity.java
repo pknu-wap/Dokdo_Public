f9b70d4475840449c9a65a14f41e16aa60851679
김수현 <suhyun_113@naver.com>
2024-10-14 00:58:18 +0900

    Feat: inventoryItemsEntity 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Entity/InventoryItemsEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/InventoryItemsEntity.java
@@ -1,6 +1,17 @@
 package com.example.rememberdokdo.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "InventoryItems")
+@Getter
+@Setter
 public class InventoryItemsEntity {
+    @Id
     private Integer inventoryItemsId; // 인벤토리 아이템 식별자
     private Integer inventoryId; // 인벤토리 식별자
     private Integer itemsId; // 아이템 식별자
