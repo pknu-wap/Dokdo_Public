28933fc8e599c5585755beaa389ab47862e8622d
김수현 <suhyun_113@naver.com>
2024-10-14 16:20:37 +0900

    Feat: 아이템 삭제 기능의 조회된 아이템 식별자 가져오기 및 아이템 이름과 아이템 설명 정보 조회 추가

--- sever/src/main/java/com/example/rememberdokdo/Service/Inventory/InventoryDeleteItemsService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/Inventory/InventoryDeleteItemsService.java
@@ -37,18 +37,24 @@ public class InventoryDeleteItemsService {
         System.out.println("인벤토리 식별자 : " + inventoryId);
 
         // 요청에서 받은 아이템 식별자로 아이템 목록에 있는 아이템인지 확인
-        ItemsEntity items = itemsRepository.findById(request.getItemId())
+        ItemsEntity item = itemsRepository.findById(request.getItemId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID의 아이템이 존재하지 않습니다.: " + request.getItemId()));
+        // 조회된 아이템 식별자 가져오기
+        Integer itemId = item.getItemId();
+        System.out.println("아이템 식별자 : " + itemId);
+        // 아이템 식별자에 대한 아이템 이름, 아이템 설명 정보 조회
+        String itemName = item.getItemName(); // 아이템 이름
+        String itemDescription = item.getItemDescription(); // 아이템 설명
 
         // 세션 식별자에 매핑된 인벤토리에서 해당 아이템이 존재하는지 확인
-        boolean itemAlreadyInInventory = inventoryItemsRepository.existsByInventoryIdAndItemsId(inventoryId, items.getItemId());
+        boolean itemAlreadyInInventory = inventoryItemsRepository.existsByInventoryIdAndItemId(inventoryId, itemId);
         if (!itemAlreadyInInventory) {
             // 아이템이 이미 인벤토리에 존재하지 않는다면 오류 메시지 반환
             throw new IllegalArgumentException("해당 아이템이 인벤토리에 존재하지 않습니다.");
         }
 
         // 아이템 삭제
-        InventoryItemsEntity inventoryItem = inventoryItemsRepository.findByInventoryIdAndItemsId(inventoryId, request.getItemId())
+        InventoryItemsEntity inventoryItem = inventoryItemsRepository.findByInventoryIdAndItemId(inventoryId, itemId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 아이템을 인벤토리에서 찾을 수 없습니다."));
 
         // 인벤토리에서 아이템 삭제
