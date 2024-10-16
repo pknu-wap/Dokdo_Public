e72b9f2e0ad9b50d7197c941cdcedb9bbad66822
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-10-16 16:53:51 +0900

    Feat: 아이템 순서 변경 구현

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -3,20 +3,35 @@ import styles from '../components/Inventory.module.css';
 import { ReactSortable } from 'react-sortablejs';
 
 function Inventory() {
-  const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
+  const [inventoryItems, setInventoryItems] = useState([
+    { id: 1, name: 'Key', action: () => alert('This is a key!') },
+    { id: 2, name: 'Map', action: () => alert('This is a map!') },
+    { id: 3, name: 'Flashlight', action: () => alert('This is a flashlight!') },
+  ]);
+
+  /* 아이템 클릭 핸들러 */
+  const handleItemClick = (item) => {
+    item.action(); /* 클릭 시 아이템의 액션 실행 */
+  };
 
   return (
     <div className={styles.InventoryBox}>
       <ReactSortable
-        list={items}
-        setList={setItems}
-        animation={150} // 150ms 애니메이션
+        list={inventoryItems}
+        setList={setInventoryItems}
+        animation={200} /* 드래그 앤 드롭 애니메이션 */
+        className={styles.InventoryList}
       >
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {inventoryItems.map((item) => (
+          <div
+            key={item.id}
+            className={styles.InventoryItem}
+            onClick={() => handleItemClick(item)} /* 아이템 클릭 시 해당 아이템의 액션 실행 */
+          >
+            {item.name}
+          </div>
         ))}
       </ReactSortable>
-      <div className={styles.InventoryItemBox}>BOX</div>
     </div>
   );
 }
