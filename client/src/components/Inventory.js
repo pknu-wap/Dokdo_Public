bcf1699fb02283a8027a840cfc4e8c7e046d1fc0
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-10-16 16:23:05 +0900

    Feat: Sortablejs 라이브러리 설치

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import styles from '../components/Inventory.module.css';
+import { ReactSortable } from 'react-sortablejs';
 
 function Inventory() {
+  const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
+
   return (
     <div className={styles.InventoryBox}>
+      <ReactSortable
+        list={items}
+        setList={setItems}
+        animation={150} // 150ms 애니메이션
+      >
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ReactSortable>
       <div className={styles.InventoryItemBox}>BOX</div>
     </div>
   );
