2349d6eae9153853b9cbdbf17ad4f112ad785478
Chaeyeon Baek <qorcodus03@naver.com>
2024-10-25 23:34:46 +0900

    Revert "Feat: 세션 유효성 검증 경로 추가"
    This reverts commit 93c6022b52d800030d1fc34f8e59106889ea4cae.

--- sever/src/main/java/com/example/rememberdokdo/Controller/SessionController.java
+++ sever/src/main/java/com/example/rememberdokdo/Controller/SessionController.java
@@ -14,16 +14,10 @@ public class SessionController {
     @Autowired
     private SessionService sessionService;
 
-    // 사용자 웹페이지 접속 시 세션 ID를 생성하고 반환하며, 기존 게임 데이터를 초기화
+    // 사용자 웹페이지 접속 시 세션 ID를 생성하고 쿠키에 저장하며, 기존 게임 데이터를 초기화
     @PostMapping("/start")
     public SessionDto startSession(HttpServletRequest request, HttpServletResponse response) {
-        // 세션을 시작하고 세션 ID를 클라이언트로 반환
+        // 세션을 시작하고, 쿠키에 저장하며, 게임 데이터를 초기화 (기능 추가 나중에 해줄 것 !)
         return sessionService.startSession(request, response);
-    }
-
-    // 세션 유효성 검증 API (세션 ID를 검증)
-    @GetMapping("/validate")
-    public boolean validateSession(@RequestHeader("Authorization") String sessionId) {
-        return sessionService.validateSession(sessionId);
     }
 }
