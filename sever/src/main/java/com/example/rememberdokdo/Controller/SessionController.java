03e8b69029dd019fc580f42b8892c6096fecbb87
Chaeyeon Baek <qorcodus03@naver.com>
2024-10-26 23:48:18 +0900

    Feat: 세션 유효 확인 엔드포인트 추가

--- sever/src/main/java/com/example/rememberdokdo/Controller/SessionController.java
+++ sever/src/main/java/com/example/rememberdokdo/Controller/SessionController.java
@@ -20,4 +20,11 @@ public class SessionController {
         // 세션을 시작하고, 쿠키에 저장하며, 게임 데이터를 초기화 (기능 추가 나중에 해줄 것 !)
         return sessionService.startSession(request, response);
     }
+
+    //세션이 유효한지 확인할 수 있는 엔드포인트
+    @GetMapping("/validate")
+    public boolean validateSession(@RequestParam String sessionId) {
+        return sessionService.validateSession(sessionId);
+    }
+
 }
