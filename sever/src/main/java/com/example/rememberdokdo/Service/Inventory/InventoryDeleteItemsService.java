bca96b64d4ba64630f3832fbfcce5fc291540fcb
김수현 <suhyun_113@naver.com>
2024-10-28 20:06:54 +0900

    Feat: 아이템 삭제 서비스 응답 DTO 생성 및 반환

--- sever/src/main/java/com/example/rememberdokdo/Service/Inventory/InventoryDeleteItemsService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/Inventory/InventoryDeleteItemsService.java
@@ -1,6 +1,7 @@
 package com.example.rememberdokdo.Service.Inventory;
 
 import com.example.rememberdokdo.Dto.Inventory.ItemDeleteRequestDto;
+import com.example.rememberdokdo.Dto.Inventory.ItemDeleteResponseDto;
 import com.example.rememberdokdo.Entity.Inventory.InventoryEntity;
 import com.example.rememberdokdo.Entity.Inventory.InventoryItemsEntity;
 import com.example.rememberdokdo.Entity.Inventory.ItemsEntity;
@@ -21,7 +22,7 @@ public class InventoryDeleteItemsService {
     private InventoryItemsRepository inventoryItemsRepository;
 
     @Transactional
-    public void deleteItem(ItemDeleteRequestDto request) {
+    public ItemDeleteResponseDto deleteItem(ItemDeleteRequestDto request) {
         // 세션 만료 여부 확인
 //        boolean sessionExpired = checkSessionExpired(request.getSessionId());
 //        if (sessionExpired) {
@@ -39,25 +40,21 @@ public class InventoryDeleteItemsService {
         // 요청에서 받은 아이템 식별자로 아이템 목록에 있는 아이템인지 확인
         ItemsEntity item = itemsRepository.findById(request.getItemId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID의 아이템이 존재하지 않습니다.: " + request.getItemId()));
-        // 조회된 아이템 식별자 가져오기
-        Integer itemId = item.getItemId();
-        System.out.println("아이템 식별자 : " + itemId);
-        // 아이템 식별자에 대한 아이템 이름, 아이템 설명 정보 조회
-        String itemName = item.getItemName(); // 아이템 이름
-        String itemDescription = item.getItemDescription(); // 아이템 설명
 
         // 세션 식별자에 매핑된 인벤토리에서 해당 아이템이 존재하는지 확인
-        boolean itemAlreadyInInventory = inventoryItemsRepository.existsByInventoryIdAndItemId(inventoryId, itemId);
-        if (!itemAlreadyInInventory) {
+        if(!inventoryItemsRepository.existsByInventoryIdAndItemId(inventoryId, item.getItemId())){
             // 아이템이 이미 인벤토리에 존재하지 않는다면 오류 메시지 반환
             throw new IllegalArgumentException("해당 아이템이 인벤토리에 존재하지 않습니다.");
         }
 
         // 아이템 삭제
-        InventoryItemsEntity inventoryItem = inventoryItemsRepository.findByInventoryIdAndItemId(inventoryId, itemId)
+        InventoryItemsEntity inventoryItem = inventoryItemsRepository.findByInventoryIdAndItemId(inventoryId, item.getItemId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 아이템을 인벤토리에서 찾을 수 없습니다."));
 
         // 인벤토리에서 아이템 삭제
         inventoryItemsRepository.delete(inventoryItem);
+
+        // 응답 DTO 생성 및 반환
+        return new ItemDeleteResponseDto(item.getItemId(), item.getItemName(), item.getItemDescription());
     }
 }
