2e0365dea91a5a04bf56969d705c457c6d6765b1
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-10-28 19:07:47 +0900

    Feat: 뉴스 이미지 FadeInOut 구현

--- client/src/pages/NewsPage.js
+++ client/src/pages/NewsPage.js
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+import New1 from '../Dokdo_Private/News1.png';
+import New2 from '../Dokdo_Private/News2.png';
+import styles from '../pages/NewsPage.module.css';
+
 function NewsPage() {
-  return <>뉴스 페이지</>;
+  const [isNews2, setIsNews2] = useState(false);
+
+  const handleNews1End = () => {
+    setIsNews2(true); // 첫 이미지 애니메이션 종료 시 두 번째 이미지 표시
+  };
+
+  return (
+    <div>
+      <img
+        src={New1}
+        className={`${styles.NewsImg} ${styles.FadeInOut} ${isNews2 ? styles.hidden : ''}`}
+        onAnimationEnd={handleNews1End}
+      />
+      {isNews2 && <img src={New2} className={`${styles.NewsImg} ${styles.FadeInOut}`} />}
+    </div>
+  );
 }
 
 export default NewsPage;
