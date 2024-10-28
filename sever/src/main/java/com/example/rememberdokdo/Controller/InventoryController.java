abf79c8d959b1a5f2b93691a394a59e230bb1637
김수현 <suhyun_113@naver.com>
2024-10-28 20:12:39 +0900

    Feat: 아이템 추가 컨트롤러 수정 및 삭제 컨트롤러 추가

--- sever/src/main/java/com/example/rememberdokdo/Controller/InventoryController.java
+++ sever/src/main/java/com/example/rememberdokdo/Controller/InventoryController.java
@@ -2,35 +2,28 @@ package com.example.rememberdokdo.Controller;
 
 import com.example.rememberdokdo.Dto.Inventory.ItemAddRequestDto;
 import com.example.rememberdokdo.Dto.Inventory.ItemAddResponseDto;
-import com.example.rememberdokdo.Entity.Inventory.ItemsEntity;
-import com.example.rememberdokdo.Repository.Inventory.ItemsRepository;
+import com.example.rememberdokdo.Dto.Inventory.ItemDeleteRequestDto;
+import com.example.rememberdokdo.Dto.Inventory.ItemDeleteResponseDto;
+import com.example.rememberdokdo.Service.Inventory.InventoryAddItemsService;
+import com.example.rememberdokdo.Service.Inventory.InventoryDeleteItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inventory")
 public class InventoryController {
     @Autowired
-    private ItemsRepository itemsRepository;
+    private InventoryAddItemsService inventoryAddItemsService;
+    @Autowired
+    private InventoryDeleteItemsService inventoryDeleteItemsService;
 
     @PostMapping(value = "/add")
-    public ItemAddResponseDto addInventory(ItemAddRequestDto requestDto) {
-        ItemAddResponseDto itemAddResponse = new ItemAddResponseDto();
-        ItemsEntity item = itemsRepository.findById(requestDto.getItemId())
-            .orElseThrow(() -> // 존재하지 않는다면 Exception이 발생
-                    new IllegalArgumentException("\"해당 ID의 아이템이 존재하지 않습니다.: \" + request.getItemId())"));
+    public ItemAddResponseDto addItem(@RequestBody ItemAddRequestDto requestDto) {
+        return inventoryAddItemsService.addItem(requestDto);
+    }
 
-
-        Integer itemId = item.getItemId();
-        String itemName = item.getItemName();
-        String itemDescription = item.getItemDescription();
-
-        itemAddResponse.setItemId(itemId);
-        itemAddResponse.setItemName(itemName);
-        itemAddResponse.setItemDescription(itemDescription);
-
-        return new ItemAddResponseDto();
+    @DeleteMapping(value = "/delete")
+    public ItemDeleteResponseDto deleteItem(@RequestBody ItemDeleteRequestDto requestDto) {
+        return inventoryDeleteItemsService.deleteItem(requestDto);
     }
 }
