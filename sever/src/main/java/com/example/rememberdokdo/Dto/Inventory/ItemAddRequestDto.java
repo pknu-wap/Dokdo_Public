ab9d8766b32b48a317eeed5ff4b5c35a0f1fe179
김수현 <suhyun_113@naver.com>
2024-10-29 23:52:51 +0900

    Feat: 기본 생성자 및 모든 필드 포함하는 생성자 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/Inventory/ItemAddRequestDto.java
@@ -1,8 +1,12 @@
 package com.example.rememberdokdo.Dto.Inventory;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor // 모든 필드를 포함하는 생성자
 public class ItemAddRequestDto {
     private String sessionId; // 세션 식별자
     private Integer itemId; // 아이템 식별자
