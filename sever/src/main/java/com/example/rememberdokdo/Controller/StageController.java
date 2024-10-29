5e11eee0b0e9bfecb190ddd864481f204a359f95
Chaeyeon Baek <qorcodus03@naver.com>
2024-10-29 16:15:52 +0900

    Feat: 스테이지 연결에 대한 엔드포인트 추가

--- sever/src/main/java/com/example/rememberdokdo/Controller/StageController.java
+++ sever/src/main/java/com/example/rememberdokdo/Controller/StageController.java
@@ -1,4 +1,26 @@
 package com.example.rememberdokdo.Controller;
 
+import com.example.rememberdokdo.Dto.StageDto;
+import com.example.rememberdokdo.Service.StageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/stage")
 public class StageController {
+
+    @Autowired
+    private StageService stageService;
+
+    // 특정 스테이지 접근 가능 여부 확인
+    @GetMapping("/{stageId}/access")
+    public StageDto checkAccess(@RequestParam String sessionId, @PathVariable("stage_id") int stageId) {
+        return stageService.checkStageAccess(sessionId, stageId);
+    }
+
+    // 스테이지 클리어 정보 저장
+    @PostMapping("/{stageId}/clear")
+    public void clearStage(@RequestParam String sessionId, @PathVariable("stage_id") int stageId) {
+        stageService.clearStage(sessionId, stageId);
+    }
 }
