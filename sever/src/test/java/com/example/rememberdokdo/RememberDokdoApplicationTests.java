6abab852d6c14fa242832156f852b7f6741d3fbd
Chaeyeon Baek <qorcodus03@naver.com>
2024-10-30 18:54:53 +0900

    Chore: 배포를 위한 수정

--- sever/src/test/java/com/example/rememberdokdo/RememberDokdoApplicationTests.java
+++ sever/src/test/java/com/example/rememberdokdo/RememberDokdoApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.rememberdokdo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RememberDokdoApplicationTests {
 
     @Test
