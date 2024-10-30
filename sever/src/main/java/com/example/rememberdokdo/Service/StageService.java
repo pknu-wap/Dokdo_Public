e0d49b7b481985ab59a48e976d2168bd064829bc
Chaeyeon Baek <qorcodus03@naver.com>
2024-10-30 19:55:21 +0900

    Feat: 스테이지1을 클리어 하지 않는 경우 접근 불가

--- sever/src/main/java/com/example/rememberdokdo/Service/StageService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/StageService.java
@@ -16,14 +16,23 @@ public class StageService {
         // 스테이지 ID가 1부터 4 사이일 경우 접근 가능
         boolean isAccessible = stageId >= 1 && stageId <= 4;
 
-        // 현재 스테이지 클리어 여부 확인
+        // 스테이지 1의 클리어 여부 확인
+        boolean isStage1Cleared = stageProgressRepository.findBySessionIdAndStageId(sessionId, 1)
+                .map(StageProgressEntity::isCleared)
+                .orElse(false);
+
+        // 스테이지 1을 클리어하지 않은 경우 다른 스테이지는 접근 불가
+        if (stageId > 1 && !isStage1Cleared) {
+            isAccessible = false;
+        }
+
+        // 현재 스테이지의 클리어 여부 확인
         boolean isCleared = stageProgressRepository.findBySessionIdAndStageId(sessionId, stageId)
                 .map(StageProgressEntity::isCleared)
                 .orElse(false);
 
         return new StageDto(stageId, isAccessible, isCleared);
     }
-
     // 스테이지 클리어 상태 저장
     public void clearStage(String sessionId, int stageId) {
         StageProgressEntity stageProgress = stageProgressRepository
@@ -45,4 +54,3 @@ public class StageService {
         stageProgressRepository.save(stageProgress);
     }
 }
-
