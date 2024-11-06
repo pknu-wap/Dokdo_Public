62392b800067aa98fa341a90bc49d9010658e8d8
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-11-07 00:15:56 +0900

    Feat: 인벤토리 context 구현

---
+++ client/src/context/InventoryContext2.js
@@ -0,0 +1,53 @@
+import React, { createContext, useContext, useState } from 'react';
+import axios from 'axios';
+
+const InventoryContext2 = createContext();
+
+export const useInventory2 = () => useContext(InventoryContext2);
+
+export const InventoryProvider2 = ({ children }) => {
+  const [items, setItems] = useState([]);
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  /* 여기 useEffect로 변경해야하나 test 팔요 */
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/session/status`);
+      setItems(response.data.inventory);
+    } catch (error) {
+      console.error('아이템을 불러오는 중 오류가 발생했습니다:', error);
+    }
+  };
+
+  /** 아이템 추가 함수
+   *  아이템은 함수 사용 시 sessionId와 itemId를 객체로 넘겨줘야함
+   *  items에 해당 아이템 이름만 저장 -> 컴포넌트에서 사진과 연동
+   */
+  const addItem = async (item) => {
+    try {
+      const response = await axios.post(`${apiUrl}/inventory/add`, item);
+      setItems((prevItems) => [...prevItems, response.data.itemName]);
+    } catch (error) {
+      console.error('아이템 추가 중 오류가 발생했습니다:', error);
+    }
+  };
+
+  /** 아이템 삭제 */
+  const deleteItem = async ({ sessionId, itemId }) => {
+    try {
+      await axios.delete(`${apiUrl}/inventory/delete`, {
+        sessionId: sessionId,
+        itemId: itemId,
+      });
+      setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+    } catch (error) {
+      console.error('아이템 삭제 중 오류가 발생했습니다:', error);
+    }
+  };
+
+  return (
+    <InventoryContext2.Provider value={{ items, addItem, deleteItem, fetchItems }}>
+      {children}
+    </InventoryContext2.Provider>
+  );
+};
--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -10,9 +10,15 @@ export const UserProvider = ({ children }) => {
   /* Session 생성 함수 */
   const createSession = async () => {
     try {
-      const response = await axios.post(`${apiUrl}/session/start`);
+      const response = await axios.post(
+        `${apiUrl}/session/start`,
+        {},
+        {
+          withCredentials: true /* 쿠키를 저장하기 위해 추가 */,
+        }
+      );
       if (response.data.sessionId) {
-        setUser({ sessionId: response.data.sessionId }); /* 여기 나중에 api 명세서 완성되면 수정 필요 */
+        setUser({ sessionId: response.data.sessionId });
       }
     } catch (error) {
       console.log('사용자 세션 생성 에러', error);
@@ -20,20 +26,26 @@ export const UserProvider = ({ children }) => {
   };
 
   /* Session에서 사용자 정보 가져오기 */
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(`${apiUrl}/sesstion/status`);
-        if (response.data.user) {
-          setUser(response.data.user);
-        }
-      } catch (error) {
-        console.log('세션 api 에러', error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         `${apiUrl}/session/status`,
+  //         {},
+  //         {
+  //           withCredentials: true,
+  //         }
+  //       );
+  //       if (response.data.user) {
+  //         setUser(response.data.user);
+  //       }
+  //     } catch (error) {
+  //       console.log('세션 api 에러', error);
+  //     }
+  //   };
 
-    fetchUser();
-  }, []);
+  //   fetchUser();
+  // }, []);
 
   return <UserContext.Provider value={{ user, setUser, createSession }}>{children}</UserContext.Provider>;
 };
