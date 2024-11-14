f0d07009093268e2cd1dd7e12767f91aa87a6197
김수현 <suhyun_113@naver.com>
2024-11-15 03:13:19 +0900

    Feat: 스테이지4 진행과정 엔티티 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Entity/Stage4ProgressEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/Stage4ProgressEntity.java
@@ -1,14 +1,26 @@
 package com.example.rememberdokdo.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Stage4Progress")
 public class Stage4ProgressEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int progressId; // PK
     private boolean isStage3Cleared;
     private int currentMissionId = 1;
     private int remainingHearts = 3;
     private boolean isCurrentMissionCleared = false;
     private boolean isGameOver = false;
+    @Column(nullable = false, unique = true)
     private String sessionId; // FK
-
-
-
 }
