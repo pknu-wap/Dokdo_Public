935b193eb49386d1cfcf79ba4bd4fc28db926bb2
김수현 <suhyun_113@naver.com>
2024-11-16 20:58:26 +0900

    Chore: 스테이지4 초기화(시작) 기능 로직 구조 작성

---
+++ sever/src/main/java/com/example/rememberdokdo/Service/Stage4ProgressService.java
@@ -0,0 +1,22 @@
+package com.example.rememberdokdo.Service;
+
+import com.example.rememberdokdo.Dto.Stage4ProgressDto;
+import com.example.rememberdokdo.Repository.Stage4ProgressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Stage4ProgressService {
+    @Autowired
+    private Stage4ProgressRepository stage4ProgressRepository;
+
+    // 초기화(시작) 기능
+    public Stage4ProgressDto startMission(Stage4ProgressDto stage4ProgressDto) {
+        // sessionId 유효성 검사 => 아마도 필요없음
+        // Stage3 클리어 여부 확인
+        // Stage4 초기화 진행
+        // DB에 새로운 Progress 정보 저장
+        // 응답 Dto 반환
+    }
+
+}
