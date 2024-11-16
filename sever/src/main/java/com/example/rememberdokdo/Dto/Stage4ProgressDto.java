6d303e470bf3bcd22c837d84a1484a383009a426
김수현 <suhyun_113@naver.com>
2024-11-16 20:16:45 +0900

    Feat: 스테이지4 진행과정 Dto 어노테이션 추가

--- sever/src/main/java/com/example/rememberdokdo/Dto/Stage4ProgressDto.java
+++ sever/src/main/java/com/example/rememberdokdo/Dto/Stage4ProgressDto.java
@@ -1,12 +1,14 @@
 package com.example.rememberdokdo.Dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor // 생성자
+@Builder
 public class Stage4ProgressDto {
     private int progressId;
     private String sessionId;
