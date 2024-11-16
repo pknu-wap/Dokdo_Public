9a197808aa25d954c5914047061fc02bfe52a734
김수현 <suhyun_113@naver.com>
2024-11-16 20:58:47 +0900

    Chore: 스테이지4 미션 도전 기능 로직 구조 작성

--- sever/src/main/java/com/example/rememberdokdo/Service/Stage4ProgressService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/Stage4ProgressService.java
@@ -19,4 +19,15 @@ public class Stage4ProgressService {
         // 응답 Dto 반환
     }
 
+    // 미션 도전 기능
+    public Stage4ProgressDto attemptMission(Stage4ProgressDto stage4ProgressDto) {
+        // 현재 미션 번호 유효성 검사 => 1,2,3만 가능
+        // 미션 성공/실패 여부에 따른 처리
+        // 성공 -> 미션 번호 증가 : 다음 미션으로 이동
+        //=> 미션 번호가 3일때, 성공하면 그대로 3으로 유지하고, 증가x(퍼즐 게임하게됨)
+        // 실패 -> 하트 개수 감소
+        // 게임 오버 확인(하트 개수 = 0)
+        // DB에 변경된 Progress 정보 저장
+        // 응답 Dto 반환
+    }
 }
