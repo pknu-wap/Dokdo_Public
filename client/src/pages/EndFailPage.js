d4840693a113993aa06ba5aa22a5568c6aa88b26
aranlll <163715414+aranlll@users.noreply.github.com>
2024-11-18 02:14:19 +0900

    Design: 실패화면 디자인 수정

--- client/src/pages/EndFailPage.js
+++ client/src/pages/EndFailPage.js
@@ -1,4 +1,6 @@
+import styles from './EndFailPage.module.css';
+
 function EndFailPage() {
-  return <div>실패</div>;
+  return <div className={styles.Back}>당신은 결국 독도를 되찾지 못하였습니다.</div>;
 }
 export default EndFailPage;
