0c4c5158090c4e819b6f8cd804897e3f5c4d3f63
김수현 <suhyun_113@naver.com>
2024-11-18 23:19:29 +0900

    Feat: sessionId가 같은 모든 레코드 삭제하는 메서드 생성

--- sever/src/main/java/com/example/rememberdokdo/Repository/Inventory/InventoryRepository.java
+++ sever/src/main/java/com/example/rememberdokdo/Repository/Inventory/InventoryRepository.java
@@ -2,6 +2,9 @@ package com.example.rememberdokdo.Repository.Inventory;
 
 import com.example.rememberdokdo.Entity.Inventory.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +14,9 @@ import java.util.Optional;
 public interface InventoryRepository extends JpaRepository<InventoryEntity, Integer> {
     // 세션 식별자와 매핑된 인벤토리 식별자 존재 여부 확인
     Optional<InventoryEntity> findBySessionId(String sessionId);
+
+    // sessionId가 같은 모든 레코드 삭제하는 메서드
+    @Modifying
+    @Query("DELETE FROM Stage4ProgressEntity p WHERE p.sessionId = :sessionId")
+    void deleteAllByInventoryId(@Param("sessionId") String sessionId);
 }
--- sever/src/main/java/com/example/rememberdokdo/Repository/StageProgressRepository.java
+++ sever/src/main/java/com/example/rememberdokdo/Repository/StageProgressRepository.java
@@ -2,6 +2,10 @@ package com.example.rememberdokdo.Repository;
 
 import com.example.rememberdokdo.Entity.StageProgressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +15,8 @@ public interface StageProgressRepository extends JpaRepository<StageProgressEnti
 
     // 특정 세션과 스테이지 ID로 엔티티 찾기
     Optional<StageProgressEntity> findBySessionIdAndStageId(String sessionId, int stageId);
-}
+
+    // sessionId가 같은 모든 레코드 삭제하는 메서드
+    @Modifying
+    @Query("DELETE FROM Stage4ProgressEntity p WHERE p.sessionId = :sessionId")
+    void deleteAllBySessionId(@Param("sessionId") String sessionId);}
