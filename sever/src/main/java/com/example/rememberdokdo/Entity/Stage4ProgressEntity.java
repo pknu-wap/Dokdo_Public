508c03a0b7429bbb2f1e3ed65fd6950858718210
김수현 <suhyun_113@naver.com>
2024-11-19 00:15:10 +0900

    Feat: 스테이지4 진행 상황 Entity에 데이터 추가

--- sever/src/main/java/com/example/rememberdokdo/Entity/Stage4ProgressEntity.java
+++ sever/src/main/java/com/example/rememberdokdo/Entity/Stage4ProgressEntity.java
@@ -20,7 +20,9 @@ public class Stage4ProgressEntity {
     private int currentMissionId = 1;
     private int remainingHearts = 3;
     private boolean isCurrentMissionCleared = false;
+    private boolean isPuzzleCleared = false;
     private boolean isGameOver = false;
+    private String message; // 게임 클리어 여부 메시지
     @Column(nullable = false, unique = true)
     private String sessionId; // FK
 }
