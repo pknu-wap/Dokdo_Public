9c671fd9ebabe610b87708971693918eda4d0ce7
[GITHub <202230431@pukyong.ac.kr>
2024-11-22 23:12:59 +0900

    FIx: gameover.js수정
    - 뒤로 가기를 방지하기 위한 코드추가

--- client/src/components/GameOver.js
+++ client/src/components/GameOver.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './GameOver.module.css';
 
-
 function GameOver() {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    /* 뒤로 가기를 방지하기 위해 popstate 이벤트 추가 */
+    const handleBackButton = (event) => {
+      event.preventDefault(); /* 기본 동작 막기 */ 
+      navigate('/'); /* 홈으로 강제 이동 */ 
+    };
+
+    window.addEventListener('popstate', handleBackButton);
+
+    return () => {
+      window.removeEventListener('popstate', handleBackButton); /* 이벤트 제거 */ 
+    };
+  }, [navigate]);
 
   return (
     <div className={styles.GameOver}>
