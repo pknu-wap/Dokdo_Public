697ab498a888ff0b6d533fefbcef978caa4be29b
김수현 <suhyun_113@naver.com>
2024-11-25 01:27:52 +0900

    Feat: StageProgressResponseDto 추가

---
+++ sever/src/main/java/com/example/rememberdokdo/Dto/StageProgressResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.rememberdokdo.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StageProgressResponseDto {
+    private Integer progressId;         // 진행 상태 ID
+    private String sessionId;       // 세션 ID
+    private int remainingHearts;    // 남은 하트
+    private boolean isCleared;      // 스테이지 클리어 여부
+}
