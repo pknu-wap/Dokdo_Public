856220ddd0d4c3bc186b7e4799643e11d22bbadb
aranlll <163715414+aranlll@users.noreply.github.com>
2024-11-26 05:14:21 +0900

    Design: 퍼즐 이미지 CSS 조정

--- client/src/pages/Stage4PuzzleGame.js
+++ client/src/pages/Stage4PuzzleGame.js
@@ -5,10 +5,10 @@ import { useInventory2 } from '../context/InventoryContext2';
 import { useUser } from 'context/UserContext';
 import { useNavigate } from 'react-router-dom';
 
-import dokdoPuzzle1 from '../assets/dokdoPuzzle1.png';
-import dokdoPuzzle2 from '../assets/dokdoPuzzle2.png';
-import dokdoPuzzle3 from '../assets/dokdoPuzzle3.png';
-import dokdoPuzzle4 from '../assets/dokdoPuzzle4.png';
+import dokdoPuzzle1 from '../assets/dokdoPuzzle1_2.png';
+import dokdoPuzzle2 from '../assets/dokdoPuzzle2_2.png';
+import dokdoPuzzle3 from '../assets/dokdoPuzzle3_2.png';
+import dokdoPuzzle4 from '../assets/dokdoPuzzle4_2.png';
 import dokdoPuzzleNone from '../assets/dokdoPuzzleNone.png';
 
 function Stage4PuzzleGame() {
@@ -24,21 +24,18 @@ function Stage4PuzzleGame() {
   const [stage4PuzzleAnswer, setStage4PuzzleAnswer] = useState(false);
 
   const [count, setCount] = useState(1000);
-  const [timerRunning, setTimerRunning] = useState(false);
+  const [timerRunning, setTimerRunning] = useState(true);
 
   const handleDropOnPuzzle1 = (e) => {
     e.preventDefault();
     const draggedItem = e.dataTransfer.getData('text/plain');
     if (draggedItem === 'dokdoPuzzle1') {
       setPuzzle1(dokdoPuzzle1);
-    }
-    if (draggedItem === 'dokdoPuzzle2') {
+    } else if (draggedItem === 'dokdoPuzzle2') {
       setPuzzle1(dokdoPuzzle2);
-    }
-    if (draggedItem === 'dokdoPuzzle3') {
+    } else if (draggedItem === 'dokdoPuzzle3') {
       setPuzzle1(dokdoPuzzle3);
-    }
-    if (draggedItem === 'dokdoPuzzle4') {
+    } else if (draggedItem === 'dokdoPuzzle4') {
       setPuzzle1(dokdoPuzzle4);
     }
   };
@@ -48,14 +45,11 @@ function Stage4PuzzleGame() {
     const draggedItem = e.dataTransfer.getData('text/plain');
     if (draggedItem === 'dokdoPuzzle1') {
       setPuzzle2(dokdoPuzzle1);
-    }
-    if (draggedItem === 'dokdoPuzzle2') {
+    } else if (draggedItem === 'dokdoPuzzle2') {
       setPuzzle2(dokdoPuzzle2);
-    }
-    if (draggedItem === 'dokdoPuzzle3') {
+    } else if (draggedItem === 'dokdoPuzzle3') {
       setPuzzle2(dokdoPuzzle3);
-    }
-    if (draggedItem === 'dokdoPuzzle4') {
+    } else if (draggedItem === 'dokdoPuzzle4') {
       setPuzzle2(dokdoPuzzle4);
     }
   };
@@ -65,14 +59,11 @@ function Stage4PuzzleGame() {
     const draggedItem = e.dataTransfer.getData('text/plain');
     if (draggedItem === 'dokdoPuzzle1') {
       setPuzzle3(dokdoPuzzle1);
-    }
-    if (draggedItem === 'dokdoPuzzle2') {
+    } else if (draggedItem === 'dokdoPuzzle2') {
       setPuzzle3(dokdoPuzzle2);
-    }
-    if (draggedItem === 'dokdoPuzzle3') {
+    } else if (draggedItem === 'dokdoPuzzle3') {
       setPuzzle3(dokdoPuzzle3);
-    }
-    if (draggedItem === 'dokdoPuzzle4') {
+    } else if (draggedItem === 'dokdoPuzzle4') {
       setPuzzle3(dokdoPuzzle4);
     }
   };
@@ -82,14 +73,11 @@ function Stage4PuzzleGame() {
     const draggedItem = e.dataTransfer.getData('text/plain');
     if (draggedItem === 'dokdoPuzzle1') {
       setPuzzle4(dokdoPuzzle1);
-    }
-    if (draggedItem === 'dokdoPuzzle2') {
+    } else if (draggedItem === 'dokdoPuzzle2') {
       setPuzzle4(dokdoPuzzle2);
-    }
-    if (draggedItem === 'dokdoPuzzle3') {
+    } else if (draggedItem === 'dokdoPuzzle3') {
       setPuzzle4(dokdoPuzzle3);
-    }
-    if (draggedItem === 'dokdoPuzzle4') {
+    } else if (draggedItem === 'dokdoPuzzle4') {
       setPuzzle4(dokdoPuzzle4);
     }
   };
@@ -185,33 +173,50 @@ function Stage4PuzzleGame() {
         <div className={styles.Timer} style={{ background: getTimerBackground() }} />
       </div>
       <div className={styles.PuzzleBack}>
+        <img className={styles.dokdoPuzzleNone} src={dokdoPuzzleNone} alt="dokdoPuzzleNone" />
         <div
-          className={`${styles.dokdoPuzzle} ${animatePuzzle1 ? styles.animate : ''}`}
+          className={`${styles.dokdoPuzzle1} ${animatePuzzle1 ? styles.animate : ''}`}
           onDragOver={(e) => e.preventDefault()}
           onDrop={handleDropOnPuzzle1}
         >
-          <img className={styles.dokdoPuzzleimg} src={puzzle1} alt="dokdopuzzle1" />
+          <img
+            className={`${styles.dokdoPuzzleimg1} ${puzzle1 === dokdoPuzzleNone ? styles.hidden : ''}`}
+            src={puzzle1}
+            alt="dokdopuzzle1"
+          />
         </div>
         <div
-          className={`${styles.dokdoPuzzle} ${animatePuzzle2 ? styles.animate : ''}`}
+          className={`${styles.dokdoPuzzle2} ${animatePuzzle2 ? styles.animate : ''}`}
           onDragOver={(e) => e.preventDefault()}
           onDrop={handleDropOnPuzzle2}
         >
-          <img className={styles.dokdoPuzzleimg} src={puzzle2} alt="dokdopuzzle2" />
+          <img
+            className={`${styles.dokdoPuzzleimg2} ${puzzle2 === dokdoPuzzleNone ? styles.hidden : ''}`}
+            src={puzzle2}
+            alt="dokdopuzzle2"
+          />
         </div>
         <div
-          className={`${styles.dokdoPuzzle} ${animatePuzzle3 ? styles.animate : ''}`}
+          className={`${styles.dokdoPuzzle3} ${animatePuzzle3 ? styles.animate : ''}`}
           onDragOver={(e) => e.preventDefault()}
           onDrop={handleDropOnPuzzle3}
         >
-          <img className={styles.dokdoPuzzleimg} src={puzzle3} alt="dokdopuzzle3" />
+          <img
+            className={`${styles.dokdoPuzzleimg3} ${puzzle3 === dokdoPuzzleNone ? styles.hidden : ''}`}
+            src={puzzle3}
+            alt="dokdopuzzle3"
+          />
         </div>
         <div
-          className={`${styles.dokdoPuzzle} ${animatePuzzle4 ? styles.animate : ''}`}
+          className={`${styles.dokdoPuzzle4} ${animatePuzzle4 ? styles.animate : ''}`}
           onDragOver={(e) => e.preventDefault()}
           onDrop={handleDropOnPuzzle4}
         >
-          <img className={styles.dokdoPuzzleimg} src={puzzle4} alt="dokdopuzzle4" />
+          <img
+            className={`${styles.dokdoPuzzleimg4} ${puzzle4 === dokdoPuzzleNone ? styles.hidden : ''}`}
+            src={puzzle4}
+            alt="dokdopuzzle4"
+          />
         </div>
       </div>
     </div>
