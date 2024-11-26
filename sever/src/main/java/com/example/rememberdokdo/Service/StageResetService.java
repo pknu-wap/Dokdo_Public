6d714a944a7c19a7f2500e08e1c38482d929c752
김수현 <suhyun_113@naver.com>
2024-11-26 11:04:17 +0900

    Refactor: 인벤토리 조회 함수 분리

--- sever/src/main/java/com/example/rememberdokdo/Service/StageResetService.java
+++ sever/src/main/java/com/example/rememberdokdo/Service/StageResetService.java
@@ -66,9 +66,7 @@ public class StageResetService {
         responseDto.setSessionId(sessionId); // sessionId에 따라 한 번 생성
 
         // 인벤토리에서 세션 ID 조회
-        InventoryEntity inventory = inventoryRepository.findBySessionId(sessionId)
-                .orElseThrow(() -> new IllegalArgumentException("인벤토리가 존재하지 않습니다."));
-        int inventoryId = inventory.getInventoryId();
+        InventoryEntity inventory = getInventoryBySessionId(sessionId);
 
         // 진행 상태 조회(가장 최근 상태)
         StageProgressEntity progressEntity = stageProgressRepository.findLatestBySessionId(sessionId)
@@ -84,7 +82,7 @@ public class StageResetService {
         }
 
         // 조건 충족 : 데이터 초기화
-        deleteAllSessionData(sessionId, inventoryId); // 모든 관련 데이터 삭제
+        deleteAllSessionData(sessionId, inventory.getInventoryId()); // 모든 관련 데이터 삭제
 
         // 실패 메시지와 함께 새로운 진행 상태 저장
         progressEntity = resetStageProgress(sessionId, "하트를 모두 소진하여 방탈출에 실패하였습니다.");
@@ -103,9 +101,7 @@ public class StageResetService {
         responseDto.setSessionId(sessionId); // sessionId에 따라 한 번 생성
 
         // 인벤토리에서 세션 ID 조회
-        InventoryEntity inventory = inventoryRepository.findBySessionId(sessionId)
-                .orElseThrow(() -> new IllegalArgumentException("인벤토리가 존재하지 않습니다."));
-        int inventoryId = inventory.getInventoryId();
+        InventoryEntity inventory = getInventoryBySessionId(sessionId);
 
         // StageProgress에서 sessionId로 isCleared 조회
         StageProgressEntity stageProgress = stageProgressRepository.findBySessionIdAndStageId(sessionId, stageId)
@@ -116,14 +112,14 @@ public class StageResetService {
 
         if (isCleared) {
             // 퍼즐 게임 성공 => 세션 ID를 포함한 데이터 삭제
-            deleteAllSessionData(sessionId, inventoryId); // 모든 관련 데이터 삭제
+            deleteAllSessionData(sessionId, inventory.getInventoryId()); // 모든 관련 데이터 삭제
             // 성공 데이터 저장
             progressEntity = resetStageProgress(sessionId, "퍼즐 게임을 클리어하여 방탈출에 성공하였습니다.");
             // 새로운 Dto에 방탈출 성공 메시지 추가
             responseDto.setMessage("퍼즐 게임을 클리어하여 방탈출에 성공하였습니다.");
         } else {
             // 퍼즐 게임 실패 => 세션 ID에 대한 데이터 초기화(세션 ID 유지)
-            deleteAllSessionData(sessionId, inventoryId); // 모든 관련 데이터 삭제
+            deleteAllSessionData(sessionId, inventory.getInventoryId()); // 모든 관련 데이터 삭제
             // 실패 데이터 저장
             progressEntity = resetStageProgress(sessionId, "퍼즐 게임을 시간 안에 클리어하지 못 해 방탈출에 실패하였습니다.");
             // 새로운 Dto에 방탈출 실패 메시지 추가
@@ -131,6 +127,12 @@ public class StageResetService {
         }
         responseDto.setProgressId(progressEntity.getProgressId());
         return responseDto;
+    }
+
+    // 인벤토리 조회
+    private InventoryEntity getInventoryBySessionId(String sessionId) {
+        return inventoryRepository.findBySessionId(sessionId)
+                .orElseThrow(() -> new IllegalArgumentException("인벤토리가 존재하지 않습니다."));
     }
 
     // 스테이지 4 진행 상황 초기화
