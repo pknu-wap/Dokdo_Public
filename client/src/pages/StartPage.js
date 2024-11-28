d61c4e108dd5658a87e1e2d74727794983e94d91
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-11-29 01:18:45 +0900

    Refactor: 인트로 뉴스 추가

--- client/src/pages/StartPage.js
+++ client/src/pages/StartPage.js
@@ -2,13 +2,16 @@ import styles from './StartPage.module.css';
 import Logo from 'assets/logo2.png';
 import { UserContext } from 'context/UserContext';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function StartPage() {
   const { createSession } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const handleStartBtnClick = async () => {
     try {
       await createSession();
+      navigate('/intro');
     } catch (error) {
       console.error('세션 생성 중 오류 발생: ', error);
     }
