d2b921f5c8898415f5acfcf0573f41fc9fd3b467
Chaeyeon Baek <qorcodus03@naver.com>
2024-11-28 14:56:22 +0900

    Chore: 배포 환경

--- sever/src/main/java/com/example/rememberdokdo/Config/CorsConfig.java
+++ sever/src/main/java/com/example/rememberdokdo/Config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000", // 로컬 개발 환경
+                "http://ec2-3-38-245-128.ap-northeast-2.compute.amazonaws.com" // 배포 환경
+        ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         config.setAllowCredentials(true); // 쿠키 전송을 위해 반드시 필요
         config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
